72448e99e3b30f7283aff2d14bcc1dabbe5c4dc1
chsahil.ram24@gmail.com <chsahil.ram24@gmail.com>
2025-01-30 11:28:53 +0530

    Day-2

---
+++ Day-2/app.js
@@ -0,0 +1,29 @@
+let str1= "double quotes";
+console.log(str1,typeof(str1));
+
+let str2= "single quotes";
+console.log(str2,typeof(str2));
+
+let str3= "temple literals";
+console.log(str3,typeof(str3));
+
+let bol1= "true";
+console.log(bol1,typeof(bol1));
+
+let bol2= "false";
+console.log(bol2,typeof(bol2));
+
+let num1=10;
+console.log(num1,typeof(num1));
+
+let num2=10.2345;
+console.log(num2,typeof(num2));
+
+let num3=10e3;
+console.log(num3,typeof(num3));
+
+let a;
+console.log(a,typeof(a));
+
+let x=null;
+console.log(x,typeof(x));
