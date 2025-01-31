a03e0b6c0dc7f2846090770a8ed9451a64a6434c
chsahil.ram24@gmail.com <chsahil.ram24@gmail.com>
2025-01-31 11:34:45 +0530

    Day-17

---
+++ Day-17/script1.js
@@ -0,0 +1,9 @@
+// export let msg ='hello Morning'
+// export function greet(){
+//     return msg +"I am coding"
+// }
+export let msg ='hello Morning'
+ function greet(){
+    return msg +"I am coding"
+}
+export default greet;
---
+++ Day-17/script2.js
@@ -0,0 +1,5 @@
+import greet, { msg } from "./script1";
+
+let message=greet()
+console.log(message)
+console.log(msg)
