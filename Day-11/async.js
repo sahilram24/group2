62a489e6b4e09d0a29cc1f523ada0f270b808d1d
chsahil.ram24@gmail.com <chsahil.ram24@gmail.com>
2025-01-31 11:10:46 +0530

    Day-11

---
+++ Day-11/async.js
@@ -0,0 +1,24 @@
+// let p=new Promise((resolve,reject)=>{
+//     setTimeout(() => {
+//         resolve('success')
+//     }, 4000);
+// });
+
+// async function demo(){
+//     console.log('start');
+//     let x = await p;
+//     console.log(x);
+//     console.log("end");
+// }
+
+// demo();
+
+// async function fetchUsers(){
+//     let response=await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//     let data=await response.json();
+//     console.log(data);
+// }
+// fetchUsers()
+
+
+console.log(window);
