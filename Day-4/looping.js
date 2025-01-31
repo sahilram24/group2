bd21e9a444fd120e9b44635b1a502536e2e2c1cb
chsahil.ram24@gmail.com <chsahil.ram24@gmail.com>
2025-01-31 11:27:06 +0530

    commit

---
+++ Day-4/looping.js
@@ -0,0 +1,29 @@
+// for(let i=1;i<=100;i++){
+//     if(i %2 !=0){
+//         console.log(i+"is odd number");
+//     }
+//     else{
+//         console.log(i+"is even number");
+//     }
+// }
+
+// let number = parseInt(prompt("Enter number:"))
+// let ans = 1;
+// if (number < 0) {
+//     console.log("Invalid Input");
+// }
+// else if (number === 1) {
+//     console.log(1);
+// }
+// else {
+//     for (let i = 1; i <= number; i++) {
+//         ans *= i;
+//     }
+//     console.log(ans);
+// }
+
+let j=1;
+do{
+    console.log("Hello");
+    j++;
+} while(j >= 5)
